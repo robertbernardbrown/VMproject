2b4b1366431e419ad53f9acde75deaa73f2cea2a
Bob Brown <robertbernardbrown@gmail.com>
2018-09-10 21:33:06 -0400

    freezes html when sidenav is open

--- app/scripts.js
+++ app/scripts.js
@@ -1,12 +1,15 @@
-let  closeBtn = document.getElementById("closebtn");
-let  hamburgerMenu = document.getElementById("hamburger-menu");
+let closeBtn = document.getElementById("closebtn"),
+    hamburgerMenu = document.getElementById("hamburger-menu"),
+    html = document.querySelector("html");
 
 hamburgerMenu.addEventListener("click", function openMenu(){
     document.getElementById("vmSidenav").style.width = "250px";
     document.getElementById("sidenav-overlay").style.display = "inline";
+    html.style.overflow = "hidden";
 })
 
 closeBtn.addEventListener("click", function closeMenu(){
     document.getElementById("vmSidenav").style.width = "0px";
     document.getElementById("sidenav-overlay").style.display = "none";
+    html.style.overflow = "visible";
 })
