e575f2e46e5af4450c9b4b73faa490a746d44e6c
Bob Brown <robertbernardbrown@gmail.com>
2018-09-10 20:28:24 -0400

    adds overlay when sidenav opens

--- app/scripts.js
+++ app/scripts.js
@@ -3,7 +3,10 @@ let  hamburgerMenu = document.getElementById("hamburger-menu");
 
 hamburgerMenu.addEventListener("click", function openMenu(){
     document.getElementById("vmSidenav").style.width = "250px";
+    document.getElementById("sidenav-overlay").style.display = "inline";
 })
+
 closeBtn.addEventListener("click", function closeMenu(){
     document.getElementById("vmSidenav").style.width = "0px";
+    document.getElementById("sidenav-overlay").style.display = "none";
 })
